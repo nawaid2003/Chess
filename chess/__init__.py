3e6cbd5af6ea397da176408769b42a49c7c0f93b
nawaid <md.nawaidsheikh@gmail.com>
2023-11-06 01:16:34 +0530

    first commit

---
+++ chess/__init__.py
@@ -0,0 +1,8 @@
+'''
+This file is a part of My-PyChess application.
+In this file, we import the modules for easy access.
+'''
+
+
+from chess.singleplayer import main as singleplayer
+from chess.mysingleplayer import main as mysingleplayer
